c65f0696339e48f35b9c4c0a12e1539494cbb536
Aayush Raj <aayush.97raj@gmail.com>
2022-02-20 03:04:03 +0530

    first commit  message

---
+++ fullStack-BE/src/main/java/com/fullStackBE/FullStackBeApplication.java
@@ -0,0 +1,13 @@
+package com.fullStackBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackBeApplication.class, args);
+	}
+
+}
